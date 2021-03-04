bf8f7d897caa1788da60c3cc93bed95b9e6818d0
Amine <amine.zarbi@grenoble-inp.org>
2021-03-04 12:18:28 +0100

    create virgin project

---
+++ cmake-build-debug/CMakeFiles/MC_EK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MC_EK.dir/main.cpp.obj"
+  "MC_EK.exe"
+  "MC_EK.exe.manifest"
+  "MC_EK.lib"
+  "MC_EK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MC_EK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
