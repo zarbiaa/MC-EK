f651c1668b8d8f0ba4bc9f2ae28c94639ec8f1df
Amine <amine.zarbi@grenoble-inp.org>
2021-03-05 17:01:27 +0100

    modification of the .cpp file - test

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main() {
     //PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
     //pnl_rng_sseed(rng, time(NULL));
-    PnlVect *v = pnl_vect_create(4);
+    PnlVect *v = pnl_vect_create(25);
     pnl_vect_print(v);
 
 
