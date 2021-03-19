04dcad1a35c0256e86a52220f47f755b44226086
zarbiaa <amine.zarbi@etu.sorbonne-universite.fr>
2021-03-19 17:32:24 +0100

    ntd

---
+++ test/test-AmericanMC.cpp
@@ -0,0 +1,11 @@
+//
+// Created by amine on 3/19/2021.
+//
+#include <iostream>
+#include <string>
+#include <ctime>
+#include "../src/BlackScholesModel.h"
+
+using namespace std;
+
+void generate_paths
